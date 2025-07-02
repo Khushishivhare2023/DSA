9042a4d1e980ef8d612fe7cf11f3e3b5315a6095
Khushishivhare2023 <khushishivhare135@gmail.com>
2025-07-02 22:53:34 +0530

    commit

--- Binary Tree/height_of_tree.java
+++ Binary Tree/height_of_tree.java
@@ -35,25 +35,25 @@ public class height_of_tree {
         int rs=sum(root.right);
         return ls+rs+root.data;
     }
-    static class Info{
-        int d;
-        int h;
-        public Info(int d, int h){
-            this.d=d;
-            this.h=h;
-        }
-    }
-    public static Info diameter(Node root){
-        if(root==null){
-            return new Info(0,0);
-        }
-        Info Linfo=diameter(root.left);
-        Info Rinfo=diameter(root.right);
-        int d=Math.max((Math.max(Linfo.d,Rinfo.d)),Linfo.h+Rinfo.h+1);
-        int h=Math.max(Linfo.h,Rinfo.h)+1;
+    // static class Info{
+    //     int d;
+    //     int h;
+    //     public Info(int d, int h){
+    //         this.d=d;
+    //         this.h=h;
+    //     }
+    // }
+    // public static Info diameter(Node root){
+    //     if(root==null){
+    //         return new Info(0,0);
+    //     }
+    //     Info Linfo=diameter(root.left);
+    //     Info Rinfo=diameter(root.right);
+    //     int d=Math.max((Math.max(Linfo.d,Rinfo.d)),Linfo.h+Rinfo.h+1);
+    //     int h=Math.max(Linfo.h,Rinfo.h)+1;
 
-        return new Info(d,h);
-    }
+    //     return new Info(d,h);
+    // }
     public static boolean isIndentical(Node node,Node subroot){
         if(node==null && subroot==null){
             return true;
@@ -79,6 +79,79 @@ public class height_of_tree {
         }
         return isIndentical(root.left,subroot) || isIndentical(root.right,subroot);
     }
+    static class Info{
+        Node node;
+        int hd;
+
+        Info(Node node, int hd){
+            this.node=node;
+            this.hd=hd;
+        }
+    }
+    public static void topView(Node root){
+        Queue<Info> q=new LinkedList<>();
+        HashMap<Integer,Node> map=new HashMap<>();
+
+        int min=0,max=0;
+        q.add(new Info(root,0));
+        q.add(null);
+
+        while(!q.isEmpty()){
+            Info curr=q.remove();
+            if(curr==null){
+                if(q.isEmpty()){
+                    break;
+                }
+                else{
+                    q.add(null);
+                }
+            }
+            else{
+                if(!map.containsKey(curr.hd)){
+                  map.put(curr.hd, curr.node);
+                }
+                if(curr.node.left!=null){
+                   q.add(new Info(curr.node.left,curr.hd-1));
+                   min=Math.min(min,curr.hd-1);
+                }
+                if(curr.node.right!=null){
+                    q.add(new Info(curr.node.right,curr.hd+1));
+                    max=Math.max(max,curr.hd+1);
+                }
+            }
+        }
+        for(int i=min;i<=max;i++){
+            System.out.print(map.get(i).data +" ");
+        }
+        System.out.println();
+    }
+    public static void Klevel(Node root,int level,int k){
+        if(root==null){
+            return;
+        }
+        if(level==k){
+            System.out.print(root.data);
+            return;
+        }
+        Klevel(root.left, level+1, k);
+        Klevel(root.right, level+1, k);
+    }
+    public static Node lca(Node root,int n1,int n2){
+        if(root==null||root.data==n1||root.data==n2){
+            return root;
+        }
+        Node leftlca=lca(root.left, n1, n2);
+        Node rightlca=lca(root.right, n1, n2);
+
+        if(leftlca==null){
+            return rightlca;
+        }
+        if(rightlca==null){
+            return leftlca;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
         Node root=new Node(1);
         root.left= new Node(2);
@@ -93,11 +166,17 @@ public class height_of_tree {
         //System.out.println(sum(root));
         //System.out.println(diameter(root).d);
 
-        Node subroot=new Node(2);
-        subroot.left=new Node(4);
-        subroot.right=new Node(5);
+        // Node subroot=new Node(2);
+        // subroot.left=new Node(4);
+        // subroot.right=new Node(5);
+        //System.out.println(isSubtree(root,subroot));
+        // topView(root);
 
-        System.out.println(isSubtree(root,subroot));
+        // int k=3;
+        // Klevel(root, 1, k);
+        System.out.println(lca(root, 4, 6).data);
+
+
         
     }
     
