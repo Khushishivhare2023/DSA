9d2a67a73ec372eee902ec59fb286e18d1162dcc
Khushishivhare2023 <khushishivhare135@gmail.com>
2025-07-07 20:06:20 +0530

    commit

--- Binary Tree/height_of_tree.java
+++ Binary Tree/height_of_tree.java
@@ -151,6 +151,70 @@ public class height_of_tree {
         }
         return root;
     }
+    public static int lcaDist(Node root, int n){
+        if(root==null){
+            return -1;
+        }
+        if(root.data==n){
+            return 0;
+        }
+        int leftDist=lcaDist(root.left, n);
+        int rightDist=lcaDist(root.right, n);
+        if(leftDist==-1 && rightDist==-1){
+            return -1;
+        }
+        else if(leftDist==-1){
+            return rightDist+1;
+        }else{
+            return leftDist+1;
+        }
+    }
+    public static int minDist(Node root,int n1, int n2){
+        Node lca=lca(root, n1, n2);
+        int dist1=lcaDist(lca,n1);
+        int dist2=lcaDist(lca,n2);
+
+        return dist1+dist2;
+    }
+    public static int KAncestor(Node root, int n, int k){
+        if(root==null){
+            return -1;
+        }
+        if(root.data==n){
+            return 0;
+        }
+        int leftDist=KAncestor(root.left, n, k);
+        int rightDist=KAncestor(root.right, n, k);
+
+        if(leftDist==-1 && rightDist==-1){
+            return -1;
+        }
+        int max=Math.max(leftDist,rightDist);
+        if(max+1==k){
+            System.out.println(root.data);
+        }
+        return max+1;
+    }
+    public static int transform(Node root){
+        if(root==null){
+            return 0;
+        }
+        int leftchild=transform(root.left);
+        int rightchild=transform(root.right);
+        int data=root.data;
+        int newleft=root.left==null?0:root.left.data;
+        int newright=root.right==null?0:root.right.data;
+        root.data=newleft+leftchild+newright+rightchild;
+        return data;
+    }
+    public static void preOrder(Node root){
+        if(root==null){
+            return;
+        }
+        System.out.println(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
 
     public static void main(String[] args) {
         Node root=new Node(1);
@@ -174,7 +238,15 @@ public class height_of_tree {
 
         // int k=3;
         // Klevel(root, 1, k);
-        System.out.println(lca(root, 4, 6).data);
+        //System.out.println(lca(root, 4, 6).data);
+
+        // int n1=4, n2=6;
+        // System.out.println(minDist(root, n1, n2));
+
+        // int n=5, k=1;
+        // KAncestor(root, n, k);
+        transform(root);
+        preOrder(root);
 
 
         
